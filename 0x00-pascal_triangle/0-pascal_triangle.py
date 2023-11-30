7050673be0fff26827e303a6dba05ae6d12b2a69
Susan Waithera <122805048+Sue609@users.noreply.github.com>
2023-11-30 22:54:43 +0300

    using recursion to generate the pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+pascal triangle function
+"""
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of integers representing Pascal's triangle.
+    """
+    if n == 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    else:
+        triangle = pascal_triangle(n - 1)
+        last_row = triangle[-1]
+        new_row = [1] + [last_row[i] + last_row[i + 1] for i in range(len(last_row) - 1)] + [1]
+        triangle.append(new_row)
+        return triangle
+  
