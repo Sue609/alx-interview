ff59dd23d23f07418205d8941b7bed20d0f77e7e
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-16 19:55:39 +0300

    calculating minum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,7 +11,7 @@ def minOperations(n):
     '''
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
 
@@ -23,4 +23,3 @@ def minOperations(n):
             factor += 1
 
     return operations
-    
