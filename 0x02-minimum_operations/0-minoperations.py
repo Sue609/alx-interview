65d5e5cdd35ea21b0d86ecb2046db6f93ce6b600
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-16 19:53:38 +0300

    calculating minimum operations of copy and paste

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+This module introduces a function
+'''
+
+
+def minOperations(n):
+    '''
+    Method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the fule.
+    '''
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        if n % factor == 0:
+            n //= factor
+            operations += factor
+        else:
+            factor += 1
+
+    return operations
+    
