3f5663eafb31577f99676a7c8838081c8e1f62b7
Sue609 <122805048+Sue609@users.noreply.github.com>
2024-03-05 09:24:42 +0300

    Write a script that reads stdin line by line and computes metrics:

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,88 @@
 #!/usr/bin/python3
+'''A script for parsing HTTP request logs.
 '''
-script that reads stdin line by line and computes metrics
-'''
-import random
-import sys
+import re
 
 
-file_sizes = []
-status_code_count = {
-    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
     }
-line_counter = 0
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-try:
-    for line in sys.stdin:
-        line = line.strip().split()
-        if len(line) == 9:
-            ip = line[0]
-            _ = line[3]
-            status_code = int(line[7])
-            file_size = int(line[8])
-            
-            # Check if status code is valid
-            if status_code in status_code_count:
-                status_code_count[status_code] += 1
-                file_sizes.append(file_size)
-                line_counter += 1
 
-            # Calculate and print stats for every 10 lines
-            if line_counter == 10:
-                total_file_size = sum(file_sizes)
-                print(f"Total file size: {total_file_size}")
-                for code in sorted(status_code_count):
-                    if status_code_count[code] > 0:
-                        print(f"{code}: {status_code_count[code]}")
-                print()
-                line_counter = 0
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
-except KeyboardInterrupt:
-    total_file_size = sum(file_sizes)
-    print(f"Total file size: {total_file_size}")
-    for code in sorted(status_code_count):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
+
+
+if __name__ == '__main__':
+    run()
